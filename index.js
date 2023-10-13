739cd2065c52aa2a0fa7b3570d301746ad29fa5a
lgmarzoa <marzoalg@sanborns.com.mx>
2023-10-13 15:35:57 -0600

    first

--- index.js
+++ index.js
@@ -2,22 +2,57 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use('/public', express.static(`${__dirname}/public`));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const urls = [];
 
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', (req, res) => {
+  let url = req.body.url.replace(/\/*$/, '');
+  let validUrl = new URL(url).hostname;
+
+  console.log(url, validUrl, );
+
+  dns.lookup(validUrl, (err, address, family) => {
+
+    if(err){
+      res.json({ error: 'invalid url' });
+    } else {
+      if(!urls.includes(req.body.url)) {
+        urls.push(req.body.url);
+      }
+
+      res.json({
+        original_url: req.body.url,
+        short_url: urls.indexOf(req.body.url) + 1
+      });
+    }
+  });
+});
+
+app.get('/api/shorturl/:number', (req, res) => {
+  res.redirect(urls[req.params.number - 1])
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
